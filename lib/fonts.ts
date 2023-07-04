c0b8bccdd62900b6df38d4d8d3811f1e8d8f7a6c
igorNeves007 <igorneves0404@gmail.com>
2023-07-04 20:41:10 -0300

    Config | Fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,6 @@
+import { Plus_Jakarta_Sans as PlusJakarta } from "next/font/google";
+
+export const plusJakarta = PlusJakarta({
+  subsets: ["cyrillic-ext"],
+  variable: "--font-plus-jakarta",
+});
